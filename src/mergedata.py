991bb345c38097405a9409ea4e6fe17687128fd8
Julian Frattini <julian.frattini@alice-dsl.net>
2023-07-12 10:55:08 +0200

    Replaced dictionaries by lists for list of items (closes #9)

--- src/mergedata.py
+++ src/mergedata.py
@@ -3,13 +3,32 @@ import json
 
 from src.util.fileloader import read_elements
 
+def convert_dict_to_list(dictionary: dict, key_name: str = 'id') -> list:
+    """This method converts a dictionary, where each key-value pair represents one item of a list, back into an actual list.
+
+    parameters:
+        dictionary -- dict object with key-value pairs representing items of a list, where each value is a dictionary itself
+        key_name -- name of the attribute under which the dict-key shall be added to the dict-value
+
+    returns: a list of dict-values, where the previous key is added as an attribute
+    """
+    result: list = []
+
+    for key in dictionary:
+        item = dictionary[key]
+        if key_name:
+            item[key_name] = key
+        result.append(item)
+
+    return result
+
 if __name__ == "__main__":    
     versions: dict = read_elements('versions')
     extractions: dict = read_elements('extractions')
 
     result: dict = {
-        'versions': versions,
-        'extractions': extractions
+        'versions': convert_dict_to_list(versions),
+        'extractions': convert_dict_to_list(extractions)
     }
 
     with open('./merged/rqfo-data.json' , 'w') as fp:
